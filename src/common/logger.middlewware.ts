3511a9298342c7bc82e55bac3053ab3a377ff39b
IfNoise <black_russan@mail.ru>
2025-02-09 15:55:20 +0700

    изменено:      src/common/logger.middlewware.ts

--- src/common/logger.middlewware.ts
+++ src/common/logger.middlewware.ts
@@ -6,15 +6,17 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(req: Request, res: Response, next: NextFunction): void {
-    const { method, originalUrl } = req;
+    const { method, originalUrl, headers, cookies } = req;
     const start = Date.now();
 
     res.on('finish', () => {
       const { statusCode } = res;
       const duration = Date.now() - start;
-      this.logger.log(`${method} ${originalUrl} ${statusCode} - ${duration}ms`);
+      this.logger.log(`${method} ${originalUrl} ${statusCode} - ${duration}ms
+      Headers:${JSON.stringify(headers)}
+      Cookies:${JSON.stringify(cookies)}
+        `);
     });
-
     next();
   }
 }
